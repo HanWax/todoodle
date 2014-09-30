0d032828b8f033c66549d5deaa78a2261691a803
Hannah Waxman <hannah.waxman8@gmail.com>
2014-09-30 21:52:11 +0100

    Organising directory structure

--- server.js
+++ server.js
@@ -10,7 +10,9 @@
 
 	// configuration =================
 
-	mongoose.connect('mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'); 	// connect to mongoDB database on modulus.io
+	// load the config
+	var database = require('./config/database');
+	mongoose.connect(database.url); 
 
 	app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
 	app.use(morgan('dev')); 										// log every request to the console
@@ -19,66 +21,8 @@
 	app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 	app.use(methodOverride());
 
-
-	// define model =================
-	var Todo = mongoose.model('Todo', {
-		text : String
-	});
-
-	// routes ======================================================================
-
-	// api ---------------------------------------------------------------------
-	// get all todos
-	app.get('/api/todos', function(req, res) {
-
-		// use mongoose to get all todos in the database
-		Todo.find(function(err, todos) {
-
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
-				res.send(err)
-
-			res.json(todos); // return all todos in JSON format
-		});
-	});
-
-	// create todo and send back all todos after creation
-	app.post('/api/todos', function(req, res) {
-
-		// create a todo, information comes from AJAX request from Angular
-		Todo.create({
-			text : req.body.text,
-			done : false
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			// get and return all the todos after you create another
-			Todo.find(function(err, todos) {
-				if (err)
-					res.send(err)
-				res.json(todos);
-			});
-		});
-
-	});
-
-	// delete a todo
-	app.delete('/api/todos/:todo_id', function(req, res) {
-		Todo.remove({
-			_id : req.params.todo_id
-		}, function(err, todo) {
-			if (err)
-				res.send(err);
-
-			// get and return all the todos after you create another
-			Todo.find(function(err, todos) {
-				if (err)
-					res.send(err)
-				res.json(todos);
-			});
-		});
-	});
+	// load the routes
+	require('./app/routes')(app);
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
