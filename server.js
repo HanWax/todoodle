bedd90d3a08547cd10a2a8352a6c86e6624ff2a7
Hannah Waxman <hannah.waxman8@gmail.com>
2014-10-07 09:01:34 +0100

    Implementing user functionality

--- server.js
+++ server.js
@@ -3,16 +3,23 @@
 	// set up ========================
 	var express  = require('express');
 	var app      = express(); 								// create our app w/ express
+	var port     = process.env.PORT || 8080;
 	var mongoose = require('mongoose'); 					// mongoose for mongodb
 	var morgan = require('morgan'); 			// log requests to the console (express4)
+	var passport = require('passport');
+	var flash 	 = require('connect-flash');
 	var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 	var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+	var cookieParser = require('cookie-parser');
+	var bodyParser   = require('body-parser');
+	var session      = require('express-session');
 
 	// configuration =================
 
 	// load the config
 	var database = require('./config/database');
 	mongoose.connect(database.url); 
+	require('./config/passport')(passport); // pass passport for configuration
 
 	app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
 	app.use(morgan('dev')); 										// log every request to the console
@@ -20,9 +27,26 @@
 	app.use(bodyParser.json()); 									// parse application/json
 	app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 	app.use(methodOverride());
+	app.use(cookieParser()); // read cookies (needed for auth)
+	app.use(bodyParser()); // get information from html forms
+
+	// set up our express application
+
+	app.set('view engine', 'ejs'); // set up ejs for templating
+
+	// required for passport
+	app.use(session({ secret: 'mysupersecretpassword' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	app.use(flash()); // use connect-flash for flash messages stored in session
+
+	// routes ======================================================================
+	// load our routes and pass in our app and fully configured passport
+
 
 	// load the routes
-	require('./app/routes')(app);
+	require('./app/routes.js')(app, passport); 
+
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
